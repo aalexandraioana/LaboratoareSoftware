a0d79a6894f0c675ccc34726c99907c8acc94175
aalexandraioana <alexandra.oaida@gmail.com>
2026-03-24 09:49:57 +0200

    lab3

---
+++ src/ro/ulbs/proiectaresoftware/students/Application.java
@@ -0,0 +1,149 @@
+package ro.ulbs.proiectaresoftware.students;
+import java.util.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Application {
+    public static void main()
+    {
+
+        Path inStudenti = Paths.get("studenti_in.txt");
+        Path outStudenti1 = Paths.get("studenti_out.txt");
+        Path outStudenti2 = Paths.get("studenti_out_sorted.txt");
+
+        try
+        {
+            List<Student> inListaStudenti = new ArrayList<Student>();
+            List<String> outListaStudenti = new ArrayList<String>();
+
+            String studentiCititiFisier = "";
+
+            try(Scanner scanner = new Scanner(inStudenti))
+            {
+                while(scanner.hasNextLine())
+                {
+                    studentiCititiFisier = scanner.nextLine();
+
+                    String[] separaStudentiFisier = studentiCititiFisier.split(",");
+
+                    int numarMatricolCurent = Integer.parseInt(separaStudentiFisier[0]);
+                    String  prenumeCurent = separaStudentiFisier[1];
+                    String numeCurent = separaStudentiFisier[2];
+                    String formatieDeStudiuCurenta = separaStudentiFisier[3];
+
+                    Student s = new Student(numarMatricolCurent, prenumeCurent, numeCurent, formatieDeStudiuCurenta);
+
+                    inListaStudenti.add(s);
+                }
+            }
+
+            inListaStudenti.sort(Comparator.comparing(Student::getFormatieDeStudiu).thenComparing(Student::getNume));
+            //inListaStudenti.sort(Comparator.comparing(Student::getNume));
+
+            for (Student s : inListaStudenti)
+            {
+                System.out.println(s.toString());
+                outListaStudenti.add(s.toString());
+            }
+
+            Files.write(outStudenti2, outListaStudenti);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        Student s1 = new Student(112, "Ioan", "Popa", "TI21/1");
+        Student s2 = new Student(112, "Maria", "Oprea", "TI21/1");
+        Student s3 = new Student(120, "Alis", "Popa", "TI21/2");
+        Student s4 = new Student(122, "Mihai", "Vecerdea", "TI21/1");
+        Student s5 = new Student(122, "Eugen", "Uritescu", "TI21/2");
+
+        IO.println(String.format("%15s %20s %s", "numar matricol", "prenume nume", "formatie de studiu"));
+        /*IO.println(s1.toString());
+        IO.println(s2.toString());
+        IO.println(s3.toString());
+        IO.println(s4.toString());
+        IO.println(s5.toString());*/
+
+        List<Student> listStudenti = new ArrayList<Student>();
+
+        listStudenti.add(s1);
+        listStudenti.add(s2);
+        listStudenti.add(s3);
+        listStudenti.add(s4);
+        listStudenti.add(s5);
+
+        for(Student s : listStudenti)
+        {
+            IO.println(s + " ");
+        }
+
+        Student cautaStudent1 = new Student(120, "Alis", "Popa", "TI21/2");
+        Student cautaStudent2 = new Student(112, "Maria", "Popa", "TI21/1");
+
+        IO.println("Cautare pt liste varianta 1: ");
+        if(cautaStudent1.verifPrezenta(listStudenti)) //listaStudenti.contains(cautaStudent1))
+        {
+            IO.println("Studentul(a) " + cautaStudent1.nume + " " + cautaStudent1.prenume + " este in lista.");
+        }
+        else
+        {
+            IO.println("Studentul(a) " + cautaStudent1.nume + " " + cautaStudent1.prenume + " nu este in lista.");
+        }
+        if(cautaStudent2.verifPrezenta(listStudenti))
+        {
+            IO.println("Studentul(a) " + cautaStudent2.nume + " " + cautaStudent2.prenume + " este in lista.");
+        }
+        else
+        {
+            IO.println("Studentul(a) " + cautaStudent2.nume + " " + cautaStudent2.prenume + " nu este in lista.");
+        }
+
+        IO.println("Cautare pt liste varianta 2: ");
+        if(listStudenti.contains(cautaStudent1))
+        {
+            IO.println("Studentul(a) " + cautaStudent1.nume + " " + cautaStudent1.prenume + " este in lista.");
+        }
+        else
+        {
+            IO.println("Studentul(a) " + cautaStudent1.nume + " " + cautaStudent1.prenume + " nu este in lista.");
+        }
+        if(cautaStudent2.verifPrezenta3(listStudenti))
+        {
+            IO.println("Studentul(a) " + cautaStudent2.nume + " " + cautaStudent2.prenume + " este in lista.");
+        }
+        else
+        {
+            IO.println("Studentul(a) " + cautaStudent2.nume + " " + cautaStudent2.prenume + " nu este in lista.");
+        }
+
+        Set<Student> setStudenti = new HashSet<>();
+
+        setStudenti.add(s1);
+        setStudenti.add(s2);
+        setStudenti.add(s3);
+        setStudenti.add(s4);
+        setStudenti.add(s5);
+
+        IO.println("Cautare pt seturi: ");
+        if(cautaStudent1.verifPrezenta2(setStudenti))
+        {
+            IO.println("Studentul(a) " + cautaStudent1.nume + " " + cautaStudent1.prenume + " este in lista.");
+        }
+        else
+        {
+            IO.println("Studentul(a) " + cautaStudent1.nume + " " + cautaStudent1.prenume + " nu este in lista.");
+        }
+        if(cautaStudent2.verifPrezenta2(setStudenti))
+        {
+            IO.println("Studentul(a) " + cautaStudent2.nume + " " + cautaStudent2.prenume + " este in lista.");
+        }
+        else
+        {
+            IO.println("Studentul(a) " + cautaStudent2.nume + " " + cautaStudent2.prenume + " nu este in lista.");
+        }
+    }
+}
---
+++ src/ro/ulbs/proiectaresoftware/students/Student.java
@@ -0,0 +1,76 @@
+package ro.ulbs.proiectaresoftware.students;
+import java.util.*;
+
+public class Student {
+    int numarMatricol;
+    String prenume;
+    String nume;
+    String formatieDeStudiu;
+
+    public Student (int numarMatricol, String prenume, String nume, String formatieDeStudiu)
+    {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+
+    public int getNumarMatricol ()
+    {
+        return this.numarMatricol;
+    }
+
+    public String getPrenume()
+    {
+        return this.prenume;
+    }
+
+    public String getNume()
+    {
+        return this.nume;
+    }
+
+    public String getFormatieDeStudiu()
+    {
+        return this.formatieDeStudiu;
+    }
+
+    public String toString()
+    {
+        return String.format("%15d %20s %s", this.numarMatricol, this.prenume + " " + this.nume, this.formatieDeStudiu);
+    }
+
+    public boolean verifPrezenta(List<Student> lista)
+    {
+        for (Student s : lista)
+        {
+            if((s.numarMatricol == this.numarMatricol) && s.nume.equals(this.nume) && s.prenume.equals(this.prenume) && s.formatieDeStudiu.equals(this.formatieDeStudiu))
+                return true;
+        }
+
+        return false;
+    }
+
+    public boolean equals(Object o)
+    {
+        Student s = (Student) o;
+        return (s.numarMatricol == this.numarMatricol) && s.nume.equals(this.nume) && s.prenume.equals(this.prenume) && s.formatieDeStudiu.equals(this.formatieDeStudiu);
+    }
+
+    public int hashCode()
+    {
+        return Objects.hash(numarMatricol, nume, prenume, formatieDeStudiu);
+    }
+
+    public boolean verifPrezenta2 (Set<Student> set)
+    {
+        return set.contains(this);
+    }
+
+    public boolean verifPrezenta3 (List<Student> lista)
+    {
+        return lista.contains(this);
+    }
+
+
+}
